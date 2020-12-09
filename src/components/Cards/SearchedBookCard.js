8bc4ff5b1166228392afed2bcd1ea4f79fe337dc
bandstrar <rob.bandstra@gmail.com>
2020-12-09 14:06:01 -0600

    adding books to user's collection functional

--- src/components/Cards/SearchedBookCard.js
+++ src/components/Cards/SearchedBookCard.js
@@ -4,13 +4,14 @@ import {
   CardTitle,
 } from 'reactstrap';
 
-export default function BookCard({ book }) {
+export default function BookCard({ book, addNewBook }) {
   return (
     <div>
       <Card style={{ width: '18rem', margin: '20px', background: '#9b775d' }}>
-        <CardImg top width="100%" src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
+        <CardImg top width="100%" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
         <CardBody>
           <CardTitle tag="h5">{book.volumeInfo.title}</CardTitle>
+          <button className='btn btn-success' id={book.id} onClick={(e) => addNewBook(e)}>Add Book to Collection</button>
         </CardBody>
       </Card>
     </div>
